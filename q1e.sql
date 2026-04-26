9d84414c675faf470b92fa9e014e8ee9b09390e3
cdylpp <cdylpp@yahoo.com>
2026-04-26 12:00:29 -0700

    fixed scheme & syntax

--- q1e.sql
+++ q1e.sql
@@ -8,16 +8,16 @@
 
 
 
-select no, t1.noBorrowers, t2.avgBorrowers
+select no
 from (select no, count(*) as numBorrowers, type
     from customer
     join borrower on cname = name
     join loan on lno = no
-    group by no) t1, (select type, avg(q1.noBorrowers) as avgBorrowers
+    group by no) t1, (select type, avg(q1.numBorrowers) as avgBorrowers
                     from (select no, count(*) as numBorrowers, type
                     from customer
                     join borrower on cname = name
                     join loan on lno = no
                     group by no) q1
                     group by type) t2
-where t1.type = t2.type and t1.noBorrowers > t2.avgBorrowers;
+where t1.type = t2.type and t1.numBorrowers > t2.avgBorrowers;
