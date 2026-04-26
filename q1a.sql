d80e8b17d3f03a4fe87f44ad0e3bdc768600a061
cdylpp <cdylpp@yahoo.com>
2026-04-26 12:28:20 -0700

    fixed query

--- q1a.sql
+++ q1a.sql
@@ -3,6 +3,14 @@
 -- Find (customer, loan) pairs where the customer no longer satisfies the minimum credit requried by the loan.
 -- The output schema should be (name, no)
 
-select c.name, l.no
-from customer c, loan l 
-where c.credit < l.minCredit;
+
+-- find the customers for which they do not satisfy their loan reqs.
+-- find all current (name, loan) pairs
+-- filter by customers with credit < minCredit
+
+select allLoanPairs.name, allLoanPairs.no
+from (select *
+from customer
+join borrower on cname = name
+join loan on lno = no) allLoanPairs
+where allLoanPairs.credit < allLoanPairs.minCredit ;
