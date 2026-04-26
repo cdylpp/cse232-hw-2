c33a00be187edd50c385124416de84f6bbe1a04f
cdylpp <cdylpp@yahoo.com>
2026-04-26 11:57:29 -0700

    matched scheme

--- q1b.sql
+++ q1b.sql
@@ -3,7 +3,7 @@
 -- If a customer took no loans, list her name with a count of zero.
 -- The output schema shoulc be (name, loanCount).
 
-select name, coalesce(count(*), 0)
+select name, coalesce(count(*), 0) as loanCount
 from customer
 left join borrower on name = cname
 left join loan on lno = no
